0f6b3e7cec70a9f46d4d7d518783f5fe93e5f0a3
vov62 <avivovgen1@gmail.com>
2022-06-17 01:29:30 +0300

    upload project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './component/Header/Header';
+import Nav from './component/Nav/Nav'
+import About from './component/About/About'
+import Skills from './component/Skills/Skills';
+import Projects from './component/Projects/Projects';
+import Contact from './component/contact/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </>
   );
 }
 
